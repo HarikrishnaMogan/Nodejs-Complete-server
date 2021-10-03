96919f189838149e444758e32ffd4f5ea9994d4f
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2021-10-03 19:41:02 +0530

    Update usersRoute.js

--- Routes/usersRoute.js
+++ Routes/usersRoute.js
@@ -2,8 +2,9 @@ const route = require("express").Router();
 const service = require("../services/usersService");
 
 route.post("/register",service.register);
-
 route.post("/login",service.login);
+route.get("/verify/:id",service.verifyUser);
+
 
 module.exports = route;
 
